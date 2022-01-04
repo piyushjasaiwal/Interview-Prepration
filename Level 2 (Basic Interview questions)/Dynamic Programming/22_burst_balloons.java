247fd569351843599dbe24c5be1ab8116d3a1b02
piyushjasaiwal <coderpiyush123@gmail.com>
2022-01-04 14:13:24 +0530

    empty

--- Level 2 (Basic Interview questions)/Dynamic Programming/22_burst_balloons.java
+++ Level 2 (Basic Interview questions)/Dynamic Programming/22_burst_balloons.java
@@ -30,19 +30,18 @@ import java.util.*;
 class burst_balloons {
 
   public static int solution(int[] arr) {
-    //write your code here
-
-    return 0;
+      //write your code here
+      return 0;
   }
 
-  public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
-    int n = scn.nextInt();
-    int[] arr = new int[n];
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] = scn.nextInt();
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        System.out.println(solution(arr));
     }
-    System.out.println(solution(arr));
-  }
 
 }
