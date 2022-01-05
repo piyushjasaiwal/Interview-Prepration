0b28f79d837c36fd1bf11b12b83ab8736d5dab3a
piyushjasaiwal <coderpiyush123@gmail.com>
2022-01-05 23:10:32 +0530

    empty

---
+++ Level 2 (Basic Interview questions)/HashMaps and Heaps/11_count_of_substring_having_all_unique_characters.java
@@ -0,0 +1,34 @@
+/*abstract
+1. You are given a string.
+2. You have to find the count of valid substrings of the given string.
+3. Valid substring is defined as a substring that has all unique characters.
+Input Format
+A string
+Output Format
+A number representing the count of substrings having all unique characters.
+Question Video
+
+  COMMENTConstraints
+1 <= length of string <= 10^5
+Sample Input
+aabcbcdbca
+Sample Output
+24
+*/
+
+import java.util.*;
+
+class count_of_substring_having_all_unique_characters {
+
+	public static int solution(String str) {
+		// write your code here
+        
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+}
