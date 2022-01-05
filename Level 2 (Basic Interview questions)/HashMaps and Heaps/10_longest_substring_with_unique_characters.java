7b756f8b1770506d1c41caaedaeba325fd5c9c0a
piyushjasaiwal <coderpiyush123@gmail.com>
2022-01-05 15:40:19 +0530

    empty

---
+++ Level 2 (Basic Interview questions)/HashMaps and Heaps/10_longest_substring_with_unique_characters.java
@@ -0,0 +1,34 @@
+/*abstract
+1. You are given a string.
+2. You have to find the length of the longest substring of the given string that contains all non-repeating characters.
+Input Format
+A string
+Output Format
+A number representing the length of longest substring with unique characters.
+Question Video
+
+  COMMENTConstraints
+1 <= length of string <= 10^5
+Sample Input
+aabcbcdbca
+Sample Output
+4
+*/
+
+import java.util.*;
+
+class longest_substring_with_unique_characters {
+
+	public static int solution(String str) {
+		// write your code here
+        
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		String str = scn.next();
+		System.out.println(solution(str));
+	}
+
+}
