76560e7716f40c44318f05acaf98f98209e3f4db
piyushjasaiwal <coderpiyush123@gmail.com>
2022-01-05 14:31:38 +0530

    empty

--- Level 2 (Basic Interview questions)/HashMaps and Heaps/07_largest_subarray_with_contiguous_elements.java
+++ Level 2 (Basic Interview questions)/HashMaps and Heaps/07_largest_subarray_with_contiguous_elements.java
@@ -27,19 +27,22 @@ class largest_subarray_with_contiguous_elements {
 
 	public static int solution(int[] arr) {
 		// write your code here
-        Set<Integer> set = new HashSet<>();
         int max_len = 0;
         for(int i = 0;i<arr.length;i++){
             int max = arr[i];
             int min = arr[i];
-            for(int j = i;j<arr.length;j++){
-                max = Math.max(max, arr[j]);
-                min = Math.min(min, arr[j]);
+            Set<Integer> set = new HashSet<>();
+            set.add(arr[i]);
+            for(int j = i+1;j<arr.length;j++){
                 if(set.contains(arr[j])){
                     break;
                 }
 
                 set.add(arr[j]);
+                
+                max = Math.max(max, arr[j]);
+                min = Math.min(min, arr[j]);
+                
                 if(max-min == j-i){
                     max_len = Math.max(max_len, j-i+1);
                 }
